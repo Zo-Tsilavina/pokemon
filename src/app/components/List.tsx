78b5d28d0f39e6e7f6b151b17838b175fa1933ac
Zo-Tsilavina <hei.tsilavina@gmail.com>
2024-03-22 20:32:51 +0300

    fix: fixed the content of List pokemon

--- src/app/components/List.tsx
+++ src/app/components/List.tsx
@@ -1,15 +1,13 @@
 import Link from "next/link";
-import "./style/list.css"
+import "./style/list.css";
 
-export default function List(e){
-     return(
-          <>
-               <Link href={`pokemon/${e.id}`}>
-                    <div key={e.id} className="container">
-                         <img src={e.image} alt="pokemon"/>
-                         <h1>{e.content}</h1>
-                    </div>
-               </Link>
-          </>
-     )
+export default function List({ e }) {
+     return (
+          <Link href={`/pokemon/${e.id}`}>
+               <div key={e.id}>
+                    <img src={e.image} alt={e.name} />
+                    <h1>{e.name}</h1>
+               </div>
+          </Link>
+     );
 }
