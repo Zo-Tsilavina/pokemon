b692e73088487998bd465ae4930b6f2d8d92d805
Zo-Tsilavina <hei.tsilavina@gmail.com>
2024-03-22 20:35:27 +0300

    feat: add new route

---
+++ src/app/client/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import PokemonList from "@/app/components/PokemonList";
+
+export default function blogPage() {
+     return(
+          <>
+               <div className="container">
+                    <PokemonList/>
+               </div>
+          </>
+     )
+}
